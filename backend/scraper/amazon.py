891b1045287ac7eaa46f2c941df095a16deba1ec
ading2210 <ading2019@gmail.com>
2024-05-23 02:07:32 -0700

    fix scraper again with random user agent

--- backend/scraper/amazon.py
+++ backend/scraper/amazon.py
@@ -1,22 +1,54 @@
-from curl_cffi import requests
-from bs4 import BeautifulSoup
+import httpx
+import pathlib
+import random
+import lxml.html
+
+scraper_dir = pathlib.Path(__file__).resolve().parent
+ua_path = scraper_dir / "useragent.txt"
+user_agents = ua_path.read_text().split("\n")
 
 def get_reviews(url):
   headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.9",
+    "User-Agent": random.choice(user_agents),
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/jxl,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "DNT": "1",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
   }
 
-  response = requests.get(url, headers=headers, impersonate="chrome120")
-  soup = BeautifulSoup(response.content, "html.parser")
-  reviews = soup.find_all("div", class_="review")
+  response = httpx.get(url, headers=headers)
+  document = lxml.html.fromstring(response.content)
+  review_divs = document.cssselect('div[id^="customer_review-"]')
+  review_data = []
+  
+  for review_div in review_divs:
+    #since our user agent is random, sometimes we get the mobile version of the page
+    title_spans = review_div.cssselect('span[data-hook="review-title"]')
+    title_links = review_div.cssselect('a[data-hook="review-title"]')
+    if title_spans:
+      title_text = title_spans[0].text_content().strip()
+    else:
+      title_text = title_links[0].text_content().strip().split("\n")[-1].strip()
+    
+    stars_span = review_div.cssselect('span.a-icon-alt')[0]
+    stars_rating = float(stars_span.text_content().strip().split()[0])
+    body_span = review_div.cssselect('span[data-hook="review-body"]')[-1]
+    body_text = body_span.text_content().strip()
 
-  reviews_data = []
-  for review in reviews:
-    title = review.find("div", class_="review-title").text.strip() if review.find("div", class_="review-title") else "No title"
-    rating = review.find("span", class_="seo-avg-rating").text.strip() if review.find("span", class_="seo-avg-rating") else "No rating"
-    text = review.find("div", class_="review-text").text.strip() if review.find("div", class_="review-text") else "No review text"
-    reviews_data.append({"title": title, "rating": rating, "text": text})
-    #print(review, end="\n\n======\n\n")
+    review_data.append({
+      "title": title_text,
+      "rating": stars_rating,
+      "text": body_text
+    })
 
-  return reviews_data
+  return review_data
+
+if __name__ == "__main__":
+  url = "https://www.amazon.com/ASUS-Gaming-GeForce-Graphics-DisplayPort/product-reviews/B0C7JYX6LN"
+  print(get_reviews(url))
