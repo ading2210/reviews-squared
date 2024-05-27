948d6da0d61cab2d9c3fa1cb578e9ef506d06e20
Anwar <anwarrehman77@gmail.com>
2024-05-27 12:11:41 -0700

    no balls

--- frontend/contentscript.js
+++ frontend/contentscript.js
@@ -1,12 +1,15 @@
 const host_name = location.hostname;
 const path = btoa(location.href);
-const marieAntoinette = {
-  "www.amazon.com" : ["cr-product-insights-cards", ],
-}
+const ref_ids = {
+  "www.amazon.com": "customer-reviews_feature_div",
+};
 
 const iframe = document.createElement("iframe");
-document.getElementById(marieAntoinette[host_name]).replaceWith(iframe);
-
-iframe.src = "chrome-extension://" + chrome.runtime.id + "/iframe.html" + "#" + path;
+iframe.src = "chrome-extension://" + chrome.runtime.id + "/index.html" + "#" + path;
 iframe.style.width = "100%";
-iframe.style.backgroundImage = '/imgs/random-16.png'
+iframe.style.backgroundImage = '/imgs/random-16.png';
+
+const targetId = ref_ids[host_name];
+const target = document.getElementById(targetId);
+
+target.parentNode.insertBefore(iframe, target);
