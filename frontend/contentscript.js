89c175f2f16beef0853a105c4f48e77986035e3c
Anwar <anwarrehman77@gmail.com>
2024-05-27 15:56:24 -0700

    added more sites :D

--- frontend/contentscript.js
+++ frontend/contentscript.js
@@ -1,15 +1,19 @@
 const host_name = location.hostname;
 const path = btoa(location.href);
 const ref_ids = {
-  "www.amazon.com": "customer-reviews_feature_div",
+  "www.amazon.com": ["cr-product-insights-cards","customer-reviews_feature_div"],
+  "www.bestbuy.com": ["", "user-generated-content-ratings-and-reviews"],
+  "www.target.com": ["", "above-the-fold-information"]
 };
 
 const iframe = document.createElement("iframe");
 iframe.src = "chrome-extension://" + chrome.runtime.id + "/index.html" + "#" + path;
 iframe.style.width = "100%";
-iframe.style.backgroundImage = '/imgs/random-16.png';
 
-const targetId = ref_ids[host_name];
-const target = document.getElementById(targetId);
-
-target.parentNode.insertBefore(iframe, target);
+let target = document.getElementById(ref_ids[host_name][0])
+if (target) {
+  target.replaceWith(iframe);
+} else {
+  target = document.getElementById(ref_ids[host_name][1]);
+  target.parentNode.insertBefore(iframe, target);
+}
