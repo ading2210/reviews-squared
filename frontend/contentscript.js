50b255a408d8c58b6d5745357346a07b61de1b03
Anwar <anwarrehman77@gmail.com>
2024-05-29 16:21:49 -0700

    ALLEN PLEEEEEASE

--- frontend/contentscript.js
+++ frontend/contentscript.js
@@ -21,24 +21,3 @@ if (target) {
   target = document.getElementById(ref_ids[host_name][1]);
   target.parentNode.insertBefore(iframe, target);
 }
-
-const url = location.href;
-
-
-
-// iframe.contentWindow.document.getElementById("summary-text") = "Saketh has no balls";
-
-// await fetch('0.0.0.0/api/reviews', {
-//   method: "POST",
-//   body: JSON.stringify({ url })
-// })
-// .then(response => response.json())
-// .then(data => async () => {
-//     await fetch('0.0.0.0/api/generate', {
-//     method: "POST",
-//     body: data
-// })
-// .then(res => {
-//     summary_element.innerText = res;
-// })
-// });
--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,10 @@
 const url = atob(location.hash.substring(1));
 const summary_query = "Create a paragraph summary of the following reviews. Talk about the good and the bad.";
+const satisfied_query = "Make a short list of things that customers liked about the product. Each element should be abotu 3-5 words and separated by a comma and space (, )"
+const dissatisfied_query = "Make a short list of things that customers disliked about the product. Each element should be abotu 3-5 words and separated by a comma and space (, )"
+const summary_element = document.getElementById("summary-text");
+const satisfied_element = document.getElementsById("sat-content");
+const dissatisfied_element = document.getElementById("dissat-content");
 
 async function update() {
     const r1 = await fetch('http://localhost:5000/api/reviews', {
@@ -9,23 +14,54 @@ async function update() {
             "content-type": "application/json"
         }
     });
-    d1 = await r1.json();
-    const req = {
+    const reviews = await r1.json();
+
+    const summary_req = {
         summary_query,
-        d1
+        reviews
     };
-    console.log(req);
-    const r2 = await fetch('http://localhost:5000/api/generate', {
+    const satisfied_req = {
+        satisfied_query,
+        reviews
+    };
+    const dissatisfied_req = {
+        dissatisfied_query,
+        reviews
+    }
+
+    const summary = await fetch('http://localhost:5000/api/generate', {
         method: "POST",
-        body: JSON.stringify({ req }),
+        body: JSON.stringify({ summary_req }),
         headers: {
             "content-type": "application/json"
         }
     });
+    let satisfied = await fetch('http://localhost:5000/api/generate', {
+        method: "POST",
+        body: JSON.stringify({ satisfied_req }),
+        headers: {
+            "content-type": "application/json"
+        }
+    });
+    let dissatisfied = await fetch('http://localhost:5000/api/generate', {
+        method: "POST",
+        body: JSON.stringify({ dissatisfied_req }),
+        headers: {
+            "content-type": "application/json"
+        }
+    });
+    satisfied = satisfied.split(', ');
+    dissatisfied = dissatisfied.split(', ');
 
-    console.log(r2);
+    summary_element.innerText = summary;
+    for (item of satisfied) {
+        satisfied_element.innerHTML += `<li class="list-group-item>${item}</li>`;
+    }
+    for (item of dissatisfied) {
+        dissatisfied_element.innerHTML += `<li class="list-group-item>${item}</li>`;
+    }
 
-    // document.getElementById("summary-text").innerText = r2;
+    document.getElementById("loading-text").innerText = "";
 }
 
 update();
