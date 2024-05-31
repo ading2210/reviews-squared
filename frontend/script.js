5447709c11f503cbb5afb8aaf2ea48bda0f89ada
Anwar <anwarrehman77@gmail.com>
2024-05-30 22:00:44 -0700

    allen look at me again

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,7 @@
 const url = atob(location.hash.substring(1));
 const summary_query = "Create a paragraph summary of the following reviews. Talk about the good and the bad.";
-const satisfied_query = "Make a short list of things that customers liked about the product. Each element should be about 3-5 words and separated by a comma and space (, )"
-const dissatisfied_query = "Make a short list of things that customers disliked about the product. Each element should be about 3-5 words and separated by a comma and space (, )"
+const satisfied_query = "Make a short list of things that customers liked about the product. Each element should be about 3-5 words and separated by a comma and space (, )";
+const dissatisfied_query = "Make a short list of things that customers disliked about the product. Each element should be about 3-5 words and separated by a comma and space (, )";
 const summary_element = document.getElementById("summary-text");
 const satisfied_element = document.getElementById("sat-content");
 const dissatisfied_element = document.getElementById("dissat-content");
@@ -26,44 +26,38 @@ async function update() {
     const dissatisfied_req = {
         query: dissatisfied_query,
         documents: r1
-    }
+    };
 
-    const summary = await fetch('http://localhost:5000/api/generate', {
-        method: "POST",
-        body: JSON.stringify(summary_req),
-        headers: {
-            "content-type": "application/json"
-        }
-    });
-    let satisfied = await fetch('http://localhost:5000/api/generate', {
-        method: "POST",
-        body: JSON.stringify(satisfied_req),
-        headers: {
-            "content-type": "application/json"
-        }
-    });
-    let dissatisfied = await fetch('http://localhost:5000/api/generate', {
-        method: "POST",
-        body: JSON.stringify(dissatisfied_req),
-        headers: {
-            "content-type": "application/json"
-        }
-    });
-    
-    console.log(summary);
-    console.log(satisfied);
-    console.log(dissatisfied);
+    const [summary, satisfied, dissatisfied] = await Promise.all([
+        fetch('http://localhost:5000/api/generate', {
+            method: "POST",
+            body: JSON.stringify(summary_req),
+            headers: {
+                "content-type": "application/json"
+            }
+        }).then(response => response.text()),
+        fetch('http://localhost:5000/api/generate', {
+            method: "POST",
+            body: JSON.stringify(satisfied_req),
+            headers: {
+                "content-type": "application/json"
+            }
+        }).then(response => response.text()),
+        fetch('http://localhost:5000/api/generate', {
+            method: "POST",
+            body: JSON.stringify(dissatisfied_req),
+            headers: {
+                "content-type": "application/json"
+            }
+        }).then(response => response.text())
+    ]);
 
-    satisfied = satisfied.text().split(', ');
-    dissatisfied = dissatisfied.text().split(', ');
+    const satisfiedList = satisfied.split(', ');
+    const dissatisfiedList = dissatisfied.split(', ');
 
-    summary_element.innerText = summary.text();
-    for (item of satisfied) {
-        satisfied_element.innerHTML += `<li class="list-group-item>${item}</li>`;
-    }
-    for (item of dissatisfied) {
-        dissatisfied_element.innerHTML += `<li class="list-group-item>${item}</li>`;
-    }
+    summary_element.innerText = summary;
+    satisfied_element.innerHTML = satisfiedList.map(item => `<li class="list-group-item">${item}</li>`).join('');
+    dissatisfied_element.innerHTML = dissatisfiedList.map(item => `<li class="list-group-item">${item}</li>`).join('');
 
     document.getElementById("loading-text").innerText = "";
 }
